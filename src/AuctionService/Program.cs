3b6afc21e2e5544674034230060b0b217921ff0a
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2023-12-02 13:13:48 +0100

    Add: DTOs

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AuctionDbContext>(opt => {
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
