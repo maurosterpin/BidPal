4db8365a98279e661df257dba95849c95c54ef0a
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2024-08-10 15:49:31 +0200

    Add Register page

--- src/AuctionService/obj/Debug/net7.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net7.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19560222d02d7b9159f5ce4fb77eed35009543fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8eb4354f8454a45db7bec81e35fa09a36c760184")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
