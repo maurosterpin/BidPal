48b5347081e0aad7bbe7e1dd23f0d651cee78445
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2024-08-11 15:49:11 +0200

    Add yarp to GatewayService

--- src/AuctionService/obj/Debug/net7.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net7.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+937eebe8eda4f02433da0acd3f0b6a3ff32366de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1618211550a46d6c431e706fdaec980c243b0ddd")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
