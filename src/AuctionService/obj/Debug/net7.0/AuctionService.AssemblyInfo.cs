c9cf169be82aeaeb16f03f1e75a455316ab4c59a
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2024-08-13 12:50:05 +0200

    Add AuctionService dockerfile

--- src/AuctionService/obj/Debug/net7.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net7.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85f62580f1ccf3c6309477499fb05617a7e6a776")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8373ee15be79449230b53d39ffc3cc94b007a06a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
