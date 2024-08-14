4dc8d1170873f60061d562deaa92443701fe1fce
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2024-08-14 13:55:30 +0200

    Fix IdentityService docker issues

--- src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
+++ src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
@@ -16,19 +16,14 @@ public class Index : PageModel
 
     public async Task<IActionResult> OnGet()
     {
-        var localAddresses = new List<string?> { "127.0.0.1", "::1" };
-        if(HttpContext.Connection.LocalIpAddress != null)
-        {
-            localAddresses.Add(HttpContext.Connection.LocalIpAddress.ToString());
-        }
-
+        var localAddresses = new string[] { "::ffff:172.19.0.1", "127.0.0.1", "::1", HttpContext.Connection.LocalIpAddress.ToString() };
         if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress?.ToString()))
         {
             return NotFound();
         }
 
         View = new ViewModel(await HttpContext.AuthenticateAsync());
-            
+
         return Page();
     }
 }
