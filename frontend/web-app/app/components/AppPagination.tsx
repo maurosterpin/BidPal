5ed3b82066d4cc10ac8524ebee107b6e1bfc5e49
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2024-08-16 13:50:55 +0200

    Add Pagination

---
+++ frontend/web-app/app/components/AppPagination.tsx
@@ -0,0 +1,23 @@
+"use-client";
+
+import React from "react";
+import { Pagination } from "flowbite-react";
+
+type Props = {
+  currentPage: number;
+  pageCount: number;
+};
+
+export default function AppPagination({ currentPage, pageCount }: Props) {
+  const [pageNumber, setPageNumber] = React.useState(currentPage);
+  return (
+    <Pagination
+      currentPage={currentPage}
+      onChangePage={(page: React.SetStateAction<number>) => setPageNumber(page)}
+      totalPages={pageCount}
+      layout="pagination"
+      showIcons={true}
+      className="text-blue-500 mb-5"
+    />
+  );
+}
