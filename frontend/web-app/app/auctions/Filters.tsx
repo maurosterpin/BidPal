80084fb7b1d97550dbfdf1a438a6e8a7f8f4ba87
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2024-08-16 16:19:15 +0200

    Add zustand

--- frontend/web-app/app/auctions/Filters.tsx
+++ frontend/web-app/app/auctions/Filters.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { Button } from "flowbite-react";
-
-type Props = {
-  pageSize: number;
-  setPageSize: (size: number) => void;
-};
+import { useParamsStore } from "@/hooks/useParamsStore";
 
 const pageSizeButtons = [4, 8, 12];
 
-export default function Filters({ pageSize, setPageSize }: Props) {
+export default function Filters() {
+  const pageSize = useParamsStore((state) => state.pageSize);
+  const setParams = useParamsStore((state) => state.setParams);
+
   return (
     <div className="flex justify-between items-center mb-4">
       <div>
@@ -17,7 +16,7 @@ export default function Filters({ pageSize, setPageSize }: Props) {
           {pageSizeButtons.map((value, i) => (
             <Button
               key={i}
-              onClick={() => setPageSize(value)}
+              onClick={() => setParams({ pageSize: value })}
               color={`${pageSize === value ? "red" : "gray"}`}
               className="focus:ring-0"
             ></Button>
