7681183aa4d77897e1d30a9f125c42ded6b16833
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2024-08-17 13:17:17 +0200

    Add nextauth route

---
+++ frontend/web-app/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,21 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import DuendeIdentityServer6 from "next-auth/providers/duende-identity-server6";
+
+const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    DuendeIdentityServer6({
+      id: "id-server",
+      clientId: "nextApp",
+      clientSecret: "secret",
+      issuer: "http://localhost:5009",
+      authorization: { params: { scope: "openid profile auctionApp" } },
+      idToken: true,
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
