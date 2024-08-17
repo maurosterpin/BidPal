2bec70f1ffd3aab70ce1ccc2651d8dca0bf4d38f
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2024-08-17 20:47:50 +0200

    Update logo icon

--- frontend/web-app/app/nav/Logo.tsx
+++ frontend/web-app/app/nav/Logo.tsx
@@ -2,7 +2,7 @@
 
 import { usePathname, useRouter } from "next/navigation";
 import React from "react";
-import { AiOutlineCar } from "react-icons/ai";
+import { BiShoppingBag } from "react-icons/bi";
 
 export default function Logo() {
   const router = useRouter();
@@ -16,7 +16,7 @@ export default function Logo() {
       onClick={reset}
       className="flex items-center gap-2 text-3xl font-semibold text-blue-500 cursor-pointer"
     >
-      <AiOutlineCar size={34} />
+      <BiShoppingBag size={34} />
       <div>BidPal</div>
     </div>
   );
