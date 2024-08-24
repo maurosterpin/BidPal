a8648552cf233442f62f8c2ac9abd3765f47f7b9
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2024-08-24 20:20:09 +0200

    Add SignalR

--- src/NotificationService/Program.cs
+++ src/NotificationService/Program.cs
@@ -1,4 +1,5 @@
 using MassTransit;
+using NotificationService.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,7 +18,10 @@ builder.Services.AddMassTransit(x =>
         cfg.ConfigureEndpoints(context);
     });
 });
+builder.Services.AddSignalR();
 
 var app = builder.Build();
+
+app.MapHub<NotificationHub>("/notifications");
 
 app.Run();
