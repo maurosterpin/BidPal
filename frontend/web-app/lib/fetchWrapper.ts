daeb3962256727fe70f29539f5cf235ed36fef37
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2024-08-31 15:04:48 +0200

    Fix fetchWrapper

--- frontend/web-app/lib/fetchWrapper.ts
+++ frontend/web-app/lib/fetchWrapper.ts
@@ -47,7 +47,12 @@ async function getHeaders() {
 }
 async function handleResponse(response: Response) {
   const text = await response.text();
-  const data = text && JSON.parse(text);
+  let data;
+  try {
+    data = JSON.parse(text);
+  } catch (error) {
+    data = text;
+  }
 
   if (response.ok) {
     return data || response.statusText;
