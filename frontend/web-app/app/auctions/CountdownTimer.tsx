aab58567ddcaf71871743023d89e620420a9ca39
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2024-08-31 15:49:58 +0200

    Add SignalR

--- frontend/web-app/app/auctions/CountdownTimer.tsx
+++ frontend/web-app/app/auctions/CountdownTimer.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import React from "react";
+import { useBidStore } from "@/hooks/useBidsStore";
+import { usePathname } from "next/navigation";
 import Countdown, { zeroPad } from "react-countdown";
 
 type Props = {
@@ -23,18 +24,15 @@ const renderer = ({
   return (
     <div
       className={`
-                border-2 
-                border-white 
-                text-white py-1 px-2 
-                rounded-lg flex justify-center
-                ${
-                  completed
-                    ? "bg-red-600"
-                    : days === 0 && hours < 10
-                    ? "bg-amber-600"
-                    : "bg-green-600"
-                }
-            `}
+            border-2 border-white text-white py-1 px-2 rounded-lg 
+            flex justify-center 
+            ${
+              completed
+                ? "bg-red-600"
+                : days === 0 && hours < 10
+                ? "bg-amber-600"
+                : "bg-green-600"
+            }`}
     >
       {completed ? (
         <span>Auction finished</span>
@@ -48,9 +46,22 @@ const renderer = ({
 };
 
 export default function CountdownTimer({ auctionEnd }: Props) {
+  const setOpen = useBidStore((state) => state.setOpen);
+  const pathname = usePathname();
+
+  function auctionFinished() {
+    if (pathname.startsWith("/auctions/details")) {
+      setOpen(false);
+    }
+  }
+
   return (
     <div>
-      <Countdown date={auctionEnd} renderer={renderer} />
+      <Countdown
+        date={auctionEnd}
+        renderer={renderer}
+        onComplete={auctionFinished}
+      />
     </div>
   );
 }
